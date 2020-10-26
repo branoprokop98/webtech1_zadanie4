b6719da9c65d6eda03be3861b89e53638dc62fc6
Branislav <branislavprokop98@gmail.com>
2020-10-26 11:16:45 +0100

    day 2

--- javascript/mainjava.js
+++ javascript/mainjava.js
@@ -1,17 +1,68 @@
 function generateTable() {
     document.getElementById('layer').style.display = 'block';
     let table = document.getElementById('table');
+    let numberOfRow = document.getElementById('rowNumber').value;
+    let numberOfCol = document.getElementById('colNumber').value;
     let count = 1;
+    let rowCounter = 1;
+    let colCounter = 1;
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i <= numberOfRow; i++) {
         let tr = document.createElement('tr');
-        for (let j = 0; j < 10; j++) {
+        for (let j = 0; j <= numberOfCol; j++) {
             let td = document.createElement('td');
-            td.innerText = count + '.';
-            count++;
+            if(j == 0 && i == 0){
+                td.innerText = ' ';
+            }
+            else if(i == 0){
+                td.innerText = 'X = ' + (colCounter);
+                colCounter++; 
+            }
+            else if (j == 0) {
+                td.innerText = 'Y = ' + (rowCounter);
+                rowCounter++;
+            }
+            else {
+                count = i * j;
+                td.innerText = count + '.';
+            }
             tr.append(td);
         }
         table.append(tr);
+    }
+}
+
+function checkValue(id){
+    let check = document.getElementById(id).value;
+    let elem = document.getElementById(id);
+
+    if(check < 1 || check > 9){
+        elem.classList.add('is-invalid');
+        return false;
+    }
+    else if(check == " " || check == null){
+        elem.classList.add('is-invalid');
+        return false;
+    }
+    else{
+        elem.classList.remove('is-invalid');
+        return true;
+    }
+}
+
+function formCheck(){
+    let row = document.getElementById('rowNumber');
+    let col = document.getElementById('colNumber');
+
+    if(row.value < 1 || row.value > 9 || col.value < 1 || col.value > 9){
+        return false;
+    }
+    else if(row.value == " " || row.value == null || col.value == " " || col.value == null){
+        return false;
+    }
+    else{
+        generateTable();
+        return true;
     }
 }
 
@@ -22,7 +73,7 @@ function closeWindow() {
     var tableRows = elmtTable.getElementsByTagName('tr');
     var rowCount = tableRows.length;
 
-    for (var x = rowCount - 1; x > 0; x--) {
+    for (var x = rowCount - 1; x >= 0; x--) {
         elmtTable.removeChild(tableRows[x]);
     }
 }
